b2c93f1df4bb0b460ac2d89d217f84d36dd9d6fb
Joice Jessica <joicejssc@gmail.com>
2017-11-09 13:43:51 +0700

    Polyline

--- app/src/main/java/a14/xiirpl3/joicejessica/googlemapsapi4/MainActivity.java
+++ app/src/main/java/a14/xiirpl3/joicejessica/googlemapsapi4/MainActivity.java
@@ -1,6 +1,5 @@
 package a14.xiirpl3.joicejessica.googlemapsapi4;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,24 +11,13 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    static final CameraPosition MALANG = CameraPosition.builder()
-            .target(new LatLng(47.6204, -122.2491))
-            .zoom(10)
-            .bearing(0)
-            .tilt(45)
-            .build();
     static final CameraPosition COPENHAGEN = CameraPosition.builder()
             .target(new LatLng(55.676097, 12.568337))
-            .zoom(17)
-            .bearing(295)
-            .tilt(90)
-            .build();
-    static final CameraPosition GOLDCOAST = CameraPosition.builder()
-            .target(new LatLng(-28.016667, 153.400000))
-            .zoom(16)
+            .zoom(5)
             .bearing(0)
             .tilt(45)
             .build();
@@ -37,26 +25,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     GoogleMap m_map;
     boolean mapReady = false;
 
-    MarkerOptions cpg, gcs;
-
-    @Override
-    public Resources getResources() {
-        return super.getResources();
-    }
+    LatLng IND = new LatLng(-0.789275, 113.921327);
+    LatLng DNM = new LatLng(56.263920, 9.501785);
+    LatLng AUS = new LatLng(-25.274398, 133.775136);
+    LatLng USA = new LatLng(37.090240, -95.712891);
+    MarkerOptions Indonesia, Denmark, Australia, America;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cpg = new MarkerOptions()
-                .position(new LatLng(55.676097, 12.568337))
-                .title("Copenhagen, Denmark")
+        Indonesia = new MarkerOptions()
+                .position(new LatLng(-0.789275, 113.921327))
+                .title("Indonesia")
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
-
-        gcs = new MarkerOptions()
-                .position(new LatLng(-28.016667, 153.400000))
-                .title("Gold Coast, Australia")
+        Denmark = new MarkerOptions()
+                .position(new LatLng(56.263920, 9.501785))
+                .title("Denmark")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+        Australia = new MarkerOptions()
+                .position(new LatLng(-25.274398, 133.775136))
+                .title("Australia")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+        America = new MarkerOptions()
+                .position(new LatLng(37.090240, -95.712891))
+                .title("USA")
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
@@ -68,12 +62,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap map) {
         mapReady = true;
         m_map = map;
-        m_map.addMarker(cpg);
-        m_map.addMarker(gcs);
-        flyTo(MALANG);
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(COPENHAGEN));
+        map.addPolyline(new PolylineOptions().geodesic(true)
+                .add(IND)
+                .add(DNM)
+                .add(USA)
+                .add(AUS));
+        m_map.addMarker(Indonesia);
+        m_map.addMarker(Denmark);
+        m_map.addMarker(America);
+        m_map.addMarker(Australia);
     }
 
     private void flyTo(CameraPosition target) {
-        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
     }
 }
